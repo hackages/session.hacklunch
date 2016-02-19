6676f959101e4c3a68f9b4907dc1440b6bd5243e
davy engone <davy@philos.io>
2016-02-19 18:19:00 +0100

    sessions component

---
+++ components/session/index.js
@@ -0,0 +1,7 @@
+import sessionTemplate from './session.html';
+
+const session = {
+  template: sessionTemplate
+};
+
+export default session;
---
+++ components/sessions/index.js
@@ -0,0 +1,8 @@
+import sessionsTemplate from './sessions.html';
+import './sessions.css';
+
+const sessions = {
+  template: sessionsTemplate
+};
+
+export default sessions;
